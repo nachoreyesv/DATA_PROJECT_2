c96ba88466287a7270fb082efdac76fd1090742c
jarupu <javieruizpuchol@gmail.com>
2024-02-06 18:48:47 +0100

    minimo

--- Ejecutable/main.py
+++ Ejecutable/main.py
@@ -69,7 +69,7 @@ def read_kml(oferta, bucket_name, file_id, project_id, topic_name):
     kml_file = os.path.join(DOWNLOAD_FOLDER, f'{file_id}.kml')
     download_blob(bucket_name, f'{file_id}.kml', kml_file)
 
-    data = {"id_oferta": [], "punto": [], "latitude": [], "longitude": []}
+    data = {"id_oferta": [], "punto": [], "latitude": [], "longitude": [], "trayecto": []}
     datos_longitude = []
     datos_latitude = []
 
@@ -84,18 +84,15 @@ def read_kml(oferta, bucket_name, file_id, project_id, topic_name):
         coords_list = [tuple(map(float, _.split(',')))[:2] for _ in coords_str.split()]
 
         for _, coords in enumerate(coords_list):
-            
-            trayecto_desde_punto_actual = coords_list[_:]  # Obtener sublista desde el punto actual hasta el final
 
             data["id_oferta"] = oferta
             data["punto"] = _ + 1
             data["latitude"] = coords[1]
             data["longitude"] = coords[0]
-            data["trayecto"] = trayecto_desde_punto_actual
+            data["trayecto"] = coords_list
             datos_latitude.append(coords[1])
             datos_longitude.append(coords[0])
             print(data)
-            pubsub_class.publish_message(data)
             time.sleep(1)
 
 
@@ -160,7 +157,6 @@ def gen_solicitudes(num_solicitudes, project_id, topic_name, datos_latitude_tota
         data_solicitud['latitude_destino'] = random.choice(latitudes_finales)
         data_solicitud['longitude_destino'] = random.choice(longitudes_finales)
         print(data_solicitud)
-        pubsub_class.publish_message(data_solicitud)
         time.sleep(1)
 
 
