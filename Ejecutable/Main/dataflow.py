4538cc508de7b4a6caabc77e82bd2005c88e2b79
Ignacio Reyes Vázquez <reyesvazqueznacho@gmail.com>
2024-02-11 18:44:11 +0100

    [ADDED] Funcion para descartar la posibilidad de match de solicitantes ya matcheados
    [ADDED] Funcion nueva en el Dataflow.py

--- Ejecutable/Main/dataflow.py
+++ Ejecutable/Main/dataflow.py
@@ -21,36 +21,57 @@ class OutputDoFn(beam.DoFn):
         lista_solicitantes = []
         lista_matches = []
         lista_no_matches = []
+        lista_ids_solicitantes = []
 
-        if isinstance(element, dict):
-            if "id_oferta" in element:
-                lista_vehiculos.append(element)
-            else:
-                lista_solicitantes.append(element)
+        if "id_oferta" in element:
+            lista_vehiculos.append(element)
         else:
-            logging.warning(f"Elemento no es un diccionario: {element}")
+            lista_solicitantes.append(element)
+
+        with open('ids_solicitantes.txt', 'r') as file:
+            ids = file.readlines()
+            lista_ids_solicitantes = [eval(i.strip()) for i in ids]
+
+        ids_lista_solicitantes = {i['id_sol'] for i in lista_ids_solicitantes}
+        lista_solicitantes = [i for i in lista_solicitantes if i['id_sol'] not in ids_lista_solicitantes]
 
         for i in lista_solicitantes:
             for e in lista_vehiculos:
                 if (i["longitude_destino"] == e["longitude_destino"]) and (i["latitude_destino"] == e["latitude_destino"]):
-                    if ((i["longitude"] - e["longitude"]) < 0.8) and ((i["latitude"] - e["latitude"]) < 0.8):
+                    if ((i["longitude"] - e["longitude"]) < 0.003) and ((i["latitude"] - e["latitude"]) < 0.003):
                         print(f'El usuario: {i["id_persona"]} ha hecho match con el coche: {e["id_coche"]}')
                         print(i)
-                        records = {'id1': i["id_solicitante"], 'id2': e["id_oferta"], 'latitude1': i['latitude'],
-                                   'longitude1': i['longitude'], 'latitude2': i['latitude'],
-                                   'longitude2': i['longitude'], 'latitude_final1': i['latitude_destino'],
-                                   'longitude_final1': i['longitude_destino'], 'latitude_final2': i['latitude_destino'],
-                                   'longitude_final2': i['longitude_destino'], 'match': 'yes'}
+                        records = {'id_solicitante': i["id_solicitante"], 'id_vehiculo': e["id_oferta"], 'latitud_solicitante': i['latitude'],
+                                   'longitud_solicitante': i['longitude'], 'latitud_vehiculo': i['latitude'],
+                                   'longitud_vehiculo': i['longitude'], 'latitud_final_solicitante': i['latitude_destino'],
+                                   'longitud_final_solicitante': i['longitude_destino'], 'latitud_final_vehiculo': i['latitude_destino'],
+                                   'longitud_final_vehciulo': i['longitude_destino'], 'match': 'yes'}
                         lista_matches.append(records)
+                        for i in lista_matches:
+                            lista_ids_solicitantes.append(i['id_solcitante'])
+                        
                     else:
-                        print('No se ha hecho match')
+                        print(f'El usuario: {i["id_persona"]} NO! ha hecho match con el coche: {e["id_coche"]}')
                         print(i, e)
-                        records = {'id1': i["id_solicitante"], 'id2': e["id_oferta"], 'latitude1': i['latitude'],
-                                   'longitude1': i['longitude'], 'latitude2': e['latitude'],
-                                   'longitude2': e['longitude'], 'latitude_final1': i['latitude_destino'],
-                                   'longitude_final1': i['longitude_destino'], 'latitude_final2': e['latitude_destino'],
-                                   'longitude_final2': e['longitude_destino'], 'match': 'no'}
+                        records = {'id_solicitante': i["id_solicitante"], 'id_vehiculo': e["id_oferta"], 'latitud_solicitante': i['latitude'],
+                                   'longitud_solicitante': i['longitude'], 'latitud_vehiculo': e['latitude'],
+                                   'longitud_vehiculo': e['longitude'], 'latitud_final_solicitante': i['latitude_destino'],
+                                   'longitud_final_solicitante': i['longitude_destino'], 'latitud_final_vehiculo': e['latitude_destino'],
+                                   'longitud_final_vehciulo': e['longitude_destino'], 'match': 'no'}
                         lista_no_matches.append(records)
+                else:
+                    print(f'El usuario: {i["id_persona"]} NO! ha hecho match con el coche: {e["id_coche"]}')
+                    print(i, e)
+                    records = {'id_solicitante': i["id_solicitante"], 'id_vehiculo': e["id_oferta"], 'latitud_solicitante': i['latitude'],
+                                   'longitud_solicitante': i['longitude'], 'latitud_vehiculo': e['latitude'],
+                                   'longitud_vehiculo': e['longitude'], 'latitud_final_solicitante': i['latitude_destino'],
+                                   'longitud_final_solicitante': i['longitude_destino'], 'latitud_final_vehiculo': e['latitude_destino'],
+                                   'longitud_final_vehciulo': e['longitude_destino'], 'match': 'no'}
+                    lista_no_matches.append(records)
+
+        with open('ids_solicitantes.txt', 'w') as file:
+            for id_solicitante in lista_ids_solicitantes:
+                file.write(id_solicitante + '\n')
 
         yield lista_matches, lista_no_matches
 
@@ -67,14 +88,14 @@ def run():
             p 
             | "ReadFromPubSub" >> beam.io.ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_ofertas}')
             | "Decode msg" >> beam.Map(decode_message)
-            | "Window1" >> beam.WindowInto(beam.window.FixedWindows(30))
+            | "Window1" >> beam.WindowInto(beam.window.FixedWindows(2))
         )
 
         p2 = (
             p 
             | "ReadFromPubSub2" >> beam.io.ReadFromPubSub(subscription=f'projects/{project_id}/subscriptions/{subscription_name_solicitudes}')
             | "Decode msg2" >> beam.Map(decode_message)
-            | "Window2" >> beam.WindowInto(beam.window.FixedWindows(30))
+            | "Window2" >> beam.WindowInto(beam.window.FixedWindows(2))
         )
 
         data = (p1, p2)
